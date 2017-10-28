a29eed21e3258c5cd173dcf68016bbc927931a96
Dmitry Chmerenko <dchmerenko@gmail.com>
2017-10-29 00:13:02 +0300

    add whitespace after semicolon

--- ch04_code_reuse/mymodule/vsearch.py
+++ ch04_code_reuse/mymodule/vsearch.py
@@ -1,3 +1,3 @@
-def search4letters(phrase:str, letters:str='aieou') -> set:
+def search4letters(phrase: str, letters: str='aieou') -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
