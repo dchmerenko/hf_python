2333c94982d47696d6591a4df2f92c680ca6950c
Dmitry Chmerenko <dchmerenko@gmail.com>
2017-10-31 22:49:58 +0300

    ready to deploy to PythonAnywhere

--- ch05_web_app/webapp/vsearch4web.py
+++ ch05_web_app/webapp/vsearch4web.py
@@ -3,14 +3,13 @@ from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello word from Flask!'
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the web!')
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -20,10 +19,11 @@ def do_search() -> 'html':
     results = str(search4letters(phrase, letters))
     return render_template(
         'results.html',
-        the_phrase = phrase,
-        the_letters = letters,
-        the_title = title,
-        the_results = results,
+        the_phrase=phrase,
+        the_letters=letters,
+        the_title=title,
+        the_results=results,
     )
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
