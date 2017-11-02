d0923a8c0520e6b051f26a690f30c37113d11269
Dmitry Chmerenko <dchmerenko@gmail.com>
2017-11-02 13:25:19 +0300

    ds10

--- ch04_code_reuse/mymodules/vsearch.py
+++ ch04_code_reuse/mymodules/vsearch.py
@@ -7,8 +7,3 @@ def search4vowels(phrase:str) -> set:
 def search4letters(phrase:str, letters:str) -> set:
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
-
-
-if __name__ == "__main__":
-    phrase = input('Provide a phrase to search for letters: ')
-    print(search4letters(phrase, 'abcd'))
--- ch04_code_reuse/vsearch.py
+++
@@ -1,8 +0,0 @@
-def search4vowels(phrase:str) -> set:
-    """Return any vowels found in supplied phrase."""
-    vowels = set('aeiou')
-    return vowels.intersection(set(phrase))
-
-if __name__ == "__main__":
-    phrase = input('Provide a phrase to search for vowels: ')
-    print(search4vowels(phrase))
